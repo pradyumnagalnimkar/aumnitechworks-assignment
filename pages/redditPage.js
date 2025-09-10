a116a9e593a6954c27db9cf237dda0aa96d22aab
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-09-10 21:05:08 +0530

    - page methods for reddit page

--- pages/redditPage.js
+++ pages/redditPage.js
@@ -1,25 +1,42 @@
+import { test} from "@playwright/test";
+import { WebUtils } from "../utils/web-utils.js";
+
 class RedditPage {
     constructor(page){
         this.page = page;
         this.searchPostTextbox = this.page.locator("reddit-search-large input[autocomplete='off']");
-        
+        this.posts = this.page.locator("#main-content a[data-testid='post-title']")
     }
 
     /**
-     * Navigate to the Reddit homepage
+     * @description - Navigate to the Reddit homepage
      */
-    async goto(){
-        await this.page.goto("https://www.reddit.com/");
+    async openApp(){
+        await test.step(`Open Reddit`, async () => {
+            await this.page.goto("https://www.reddit.com/");
+        });
     }
 
     /**
-     * 
-     * @param {*} subreddit - Subreddit to search for
+     * @description - Search for a subreddit
+     * @param {string} subreddit - Subreddit to search for
      */
     async search(subreddit){
-        await this.searchPostTextbox.fill(subreddit);
-        await this.page.keyboard.press("Enter");
+        await test.step(`Search for ${subreddit}`, async () => {
+            await this.searchPostTextbox.fill(subreddit);
+            await this.page.keyboard.press("Enter");
+        });
     }
 
-
+    /**
+     * @description - Keep scrolling until the number of posts is reached
+     * @param {number} numberOfPosts - Number of posts to load
+     */
+    async loadPosts(numberOfPosts){
+        await test.step(`Load ${numberOfPosts} posts`, async () => {
+            while (await this.posts.count() < numberOfPosts) {
+                await WebUtils.scrollToBottom(this.page);
+            }
+        });
+    }
 }
