6180be62f71479b4ff0f5253cc74ccf1d5d121c0
Pradyumna Galnimkar <pradyumna.galnimkar@gmail.com>
2025-09-10 22:57:10 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
     trace: 'on-first-retry',
   },
 
@@ -40,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
